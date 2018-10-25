c275eeaa16633cfd4e00dfa3600cec0adb327f9a
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-10-25 11:27:03 -0300

    resolucao do problema 7

---
+++ fundamentosPython/etapa2/problema7.py
@@ -0,0 +1,23 @@
+#Escreva um programa em Python que armazene em uma tupla o nome em inglês de distintas cores. Utilize essa tupla para trocar a cor da tartaruga como a imagem
+
+import turtle
+
+cores = ('red','yellow','orange','green','blue','purple')
+
+
+
+retas = int(input('Quantas retas deseja que tenha a imagem? '))
+
+
+for i in range(0,retas+1):
+    turtle.forward(i+1)
+    turtle.left(61)
+    turtle.speed('fast')
+    
+    turtle.color(cores[i%6])
+    
+        
+
+
+
+    
