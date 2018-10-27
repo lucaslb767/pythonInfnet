c2482ce50acfffac7f4722cee476469cf471c32a
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-10-27 14:46:12 -0300

    resolucao questao 11

---
+++ fundamentosPython/TP1/questao11.py
@@ -0,0 +1,19 @@
+#11. Faça uma função no Python que, utilizando a ferramenta turtle, desenhe um triângulo de lado N.
+
+import turtle
+
+def triangulo(lado):
+    
+    angulo = 120
+    
+    turtle.speed('fastest')
+    turtle.forward(lado)
+    turtle.left(angulo)
+    turtle.forward(lado)
+    turtle.left(angulo)
+    turtle.forward(lado)
+    
+lado = int(input('Qual é o tamanho do lado do triangulo? '))
+
+triangulo(lado)
+    
