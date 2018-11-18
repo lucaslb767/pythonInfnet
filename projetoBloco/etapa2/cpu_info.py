0ae501a63ac95ea58cd51b88b2a4629f3c313b23
Lucas <lucaslbarrozo@gmail.com>
2018-11-18 17:20:56 -0200

    separa os programas de cpu e memoria

--- projetoBloco/etapa2/cpu_info.py
+++ projetoBloco/etapa2/cpu_info.py
@@ -1,6 +1,10 @@
 import pygame
 import psutil
 
+black = (0,0,0)
+white = (255,255,255)
+blue = (0,0,255)
+red = (255,0,0)
 #iniciando a janela principal
 
 screen_width = 800
@@ -13,22 +17,18 @@ pygame.display.init()
 pygame.font.init()
 font = pygame.font.Font(None, 32)
 
-#Mostra uso de memória
 
-def mostra_uso_memoria():
-    mem = psutil.virtual_memory()
+#mostra uso de CPU
+
+def mostra_uso_cpu():
+    capacity = psutil.cpu_percent(interval=0)
     larg = screen_width - 2*20
-    screen.fill((0,0,0))
-    pygame.draw.rect(screen, (0,0,255),(20,50,larg,70))
-
-    larg2 = larg*mem.percent/100
-    pygame.draw.rect(screen,(255,0,0),(20,50,larg2,70))
-    total = round(mem.total/(1024*1024*1024),2)
-
-    texto_barra = "Uso de Memória (Total: " + str(total) + "GB): "
-    text = font.render(texto_barra,1, (255,255,255))
-    screen.blit(text, (20,10))
-
+    screen.fill(black)
+    pygame.draw.rect(screen, blue, (20,110, larg, 130))
+    larg = larg*capacity/100
+    pygame.draw.rect(screen, red,(20,110, larg, 130))
+    text = font.render("Uso  de CPU: ",1, white)
+    screen.blit(text,(20,80))
 #Cria relógio
 
 clock = pygame.time.Clock()
@@ -44,7 +44,8 @@ while not terminou:
             terminou = True
 
     if cont == 60:
-        mostra_uso_memoria()
+
+        mostra_uso_cpu()
         cont = 0
 
     #atualiza o desenho na tela
---
+++ projetoBloco/etapa2/memory_info.py
@@ -0,0 +1,64 @@
+import pygame
+import psutil
+
+black = (0,0,0)
+white = (255,255,255)
+blue = (0,0,255)
+red = (255,0,0)
+#iniciando a janela principal
+
+screen_width = 800
+screen_height = 600
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Uso de memória")
+pygame.display.init()
+
+pygame.font.init()
+font = pygame.font.Font(None, 32)
+
+#Mostra uso de memória
+
+def mostra_uso_memoria():
+    mem = psutil.virtual_memory()
+    larg = screen_width - 2*20
+    screen.fill(black)
+    pygame.draw.rect(screen, blue,(20,50,larg,70))
+
+    larg2 = larg*mem.percent/100
+    pygame.draw.rect(screen,red,(20,50,larg2,70))
+    total = round(mem.total/(1024*1024*1024),2)
+
+    texto_barra = "Uso de Memória (Total: " + str(total) + "GB): "
+    text = font.render(texto_barra,1, white)
+    screen.blit(text, (20,10))
+
+
+#Cria relógio
+
+clock = pygame.time.Clock()
+
+cont = 60
+
+terminou = False
+
+while not terminou:
+    #checar os eventos do mouse aqui
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminou = True
+
+    if cont == 60:
+        mostra_uso_memoria()
+        cont = 0
+
+    #atualiza o desenho na tela
+    pygame.display.update()
+
+    #60 frames por segundo
+
+    clock.tick(60)
+    cont = cont +1
+
+#finaliza a janela
+pygame.display.quit()
