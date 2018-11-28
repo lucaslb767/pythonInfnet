18dd7b0a5057fd10c05f0d94b5bcbce2a506ebe0
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-11-28 11:53:22 -0200

    coloca .lower para aceitar qualquer tipo de entrada 'sair'

--- fundamentosPython/TP3/Q6.py
+++ fundamentosPython/TP3/Q6.py
@@ -1,13 +1,13 @@
 #Escreva um programa em Python que leia diversas frases até a palavra “Sair” ser digitada. Indique quais frases apresentam a palavra “eu”. (código)
 
-input_usuario = str(input('Digite sua frase (digite "sair" para terminar): '))
+input_usuario = str(input('Digite sua frase (digite "sair" para terminar): ').lower())
 
 lista_frases = []
 
 while input_usuario != 'sair':
 
     lista_frases.append(input_usuario)
-    input_usuario = str(input('Digite sua próxima frase (digite "sair" para terminar): '))
+    input_usuario = str(input('Digite sua próxima frase (digite "sair" para terminar): ').lower())
 
 for frases in lista_frases:
 
