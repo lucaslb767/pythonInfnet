dd62c5563e804e846c968d51a34f23cc491c3b4d
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-11-29 22:59:41 -0200

    Q11

--- fundamentosPython/TP3/Q11.py
+++ fundamentosPython/TP3/Q11.py
@@ -4,19 +4,25 @@ import pygame
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT= 500
-RADIUS = 100
 
+
+RADIUS= 100
+
+X = SCREEN_WIDTH//2
+Y = SCREEN_HEIGHT//2
 BLUE = (0,0,255)
+BLACK = (0,0,0)
 pygame.init()
+
 
 
 win = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
-pygame.display.set_caption('Q11')
+pygame.display.set_caption('Q10')
 
 
 
-vel = 5
+vel = 20
 
 run = True
 while run:
@@ -27,7 +33,19 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    pygame.draw.circle(win,BLUE,(SCREEN_WIDTH//2 , SCREEN_HEIGHT//2 ), RADIUS )
+    keys = pygame.key.get_pressed()
+
+    X += vel
+
+    #controla colisao
+
+    if X > SCREEN_WIDTH - 50 :
+        X = 0
+    if X < 0 :
+        X = SCREEN_WIDTH - 50
+
+    win.fill(BLACK)
+    pygame.draw.circle(win,BLUE,(X, Y ), RADIUS )
     pygame.display.update()
 
 pygame.display.quit()
